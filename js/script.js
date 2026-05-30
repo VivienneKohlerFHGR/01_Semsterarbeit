a6b2227bfd41888dfa4dbd88645faae03863f8ad
aaronkaiser1999 <aaron.kaiser@stud.fhgr.ch>
2026-05-30 06:19:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("blub");
+async function loadData() {
+    const url = 'https://api.parkendd.de/Zuerich';
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const data = await loadData();
+console.log(data); // gibt die Daten der API oder false in der Konsole aus
